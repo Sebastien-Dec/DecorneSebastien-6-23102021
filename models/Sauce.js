351952ac672a2562f6573a44a8a92e7953c725ae
decorne <sebastiendecorne@gmail.com>
2021-11-04 17:49:28 +0100

    comment Sauce

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//Creation of the data schema for the sauces
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
